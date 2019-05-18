faf1fe275d8e61ce4225a9f734f76f2e592ce9a8
Charisse Strong <msstrong92@gmail.com>
2019-05-18 13:40:05 -0400

    navbar

--- streamline/src/App.js
+++ streamline/src/App.js
@@ -5,18 +5,28 @@ import Explore from './components/Explore'
 import Following from './components/Following'
 import {Search} from 'semantic-ui-react'
 import logo from './images/logo-horizontal.png'
+import chat from './images/icon-chat@2x.png'
+import explore from './images/icon-explore.png'
+import star from './images/star.png'
 class App extends Component {
 
   render() {
     return (
       <div className="landing-background">
         <nav className="nav">
-          <Link className="link" to="/somewhere">&#9776;</Link>
+          <Link className="link menu" to="/somewhere">&#9776;</Link>
           <Link className="link" to="/">Home</Link>
-          <Link className="link" to="/explore">Explore</Link>
-          <Link className="link" to="/following">Following</Link>
-          <img />
+          <Link className="link" to="/explore">Explore<img src={explore} /></Link>
+          <Link className="link" to="/following">Following<img src={star}/></Link>
+
+          <div className="logo">
+            <img src={logo} />
+          </div>
+
+          <div className="search">
           <Search placeholder="Search"/>
+          </div>
+          <img className="chat" src={chat} />
         </nav>
 
         <Route exact path='/' render={()=><Landing/>}/>
