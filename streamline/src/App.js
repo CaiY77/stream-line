c1430f5a5da7cab82c28f010d19de8369b406cde
Cai Yang <caih.yang@gmail.com>
2019-05-18 10:52:14 -0400

    nav bar

--- streamline/src/App.js
+++ streamline/src/App.js
@@ -3,25 +3,28 @@ import {Route, Link} from 'react-router-dom'
 import Landing from './components/Landing'
 import Explore from './components/Explore'
 import Following from './components/Following'
+import {Search} from 'semantic-ui-react'
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/explore">Explore</Link>
-          <Link to="/following">Following</Link>
+        <nav className="nav">
+          <Link className="link" to="/somewhere">&#9776;</Link>
+          <Link className="link" to="/">Home</Link>
+          <Link className="link" to="/explore">Explore</Link>
+          <Link className="link" to="/following">Following</Link>
+          <Search placeholder="Search..."/>
         </nav>
 
-        <Route exact path='/' render={()=><Landing/>}/>
-        <Route path='/explore' render={()=><Explore/>}/>
-        <Route path='/following' render={()=><Following/>}/>
-      </div>
-    );
-  }
+          <Route exact path='/' render={()=><Landing/>}/>
+          <Route path='/explore' render={()=><Explore/>}/>
+          <Route path='/following' render={()=><Following/>}/>
+          </div>
+          );
+          }
 
-}
+          }
 
-export default App;
+          export default App;
--- streamline/src/components/Landing.js
+++ streamline/src/components/Landing.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import '../App.css'
 
 class Landing extends Component {
 
   render() {
     return (
-      <div>
-        <h1>something else</h1>
+      <div className="landing-background">
+
+        <iframe width="560" height="315"
+          src="https://www.youtube.com/embed/OUuhnZ4A9cg?autoplay=0&?start=885"
+          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+
       </div>
     );
   }
