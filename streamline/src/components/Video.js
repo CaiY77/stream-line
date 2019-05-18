1a2789b34621dcae2abf7b0ed5b4c57d0eb41a97
Cai Yang <caih.yang@gmail.com>
2019-05-18 13:11:04 -0400

    video box

--- streamline/src/components/Video.js
+++ streamline/src/components/Video.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import {Feed,Button} from 'semantic-ui-react'
+import {Feed,Button,Input,Divider} from 'semantic-ui-react'
 import '../App.css'
-const faker = require('faker');
+import Faker from 'faker'
+
 class Video extends Component {
 
-  render() {
+  comments = () => {
+    let howMany = [1,1,1,1,1,1,1,1,1,1,1,1,1];
 
+    let commentArr = howMany.map(who=>{
+      return (<Feed.Event>
+        <Feed.Content>
+          <Feed.User>{Faker.name.findName()} : </Feed.User>
+          <Feed.Summary>{Faker.lorem.lines()}</Feed.Summary>
+        </Feed.Content>
+      </Feed.Event>)
+    })
+
+    return commentArr;
+  }
+
+  render() {
     return (<div className="video-contain">
       <iframe width="775" height="421"
         src={this.props.link}
-        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
-
-      <Feed>
-        <Feed.Label>Someone:</Feed.Label>
-        <Feed.Content>
-          <Feed.Summary content={comment} />
-        </Feed.Content>
-      </Feed>
+      <div className="chat">
+        <h3 className="over">{Faker.lorem.sentence()}</h3>
+        <img className='img-icon' src={Faker.image.image()}/>
+        <Divider/>
+        <Feed className="feed-style">
+          {this.comments()}
+        </Feed>
+        <Input placeholder="Say something mean..." />
+      </div>
     </div>);
   }
 
