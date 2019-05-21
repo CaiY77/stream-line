26e210b08afdbe3e54d5f859c29326405f45f9e1
Cai Yang <caih.yang@gmail.com>
2019-05-20 23:31:26 -0400

    basic landing page

--- streamline/src/components/Landing.js
+++ streamline/src/components/Landing.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css'
 import Video from './Video'
+import StreamGame from './StreamGame'
 import {Feed,Button} from 'semantic-ui-react'
+import Faker from 'faker'
+
 
 const links=[
 "https://www.youtube.com/embed/OUuhnZ4A9cg?autoplay=0&?start=885",
 "https://www.youtube.com/embed/UWNoSGjt_dU?autoplay=0&?start=12",
 "https://www.youtube.com/embed/QsawpqAney0?autoplay=0&?start=6"
+]
+const stream=[
+  "https://www.youtube.com/embed/attMWXHVM40?start=2",
+  "https://www.youtube.com/embed/sgNFOHItPSk?start=6",
+  "https://www.youtube.com/embed/7Wc9EMzxvoE?start=5"
 ]
 
 class Landing extends Component {
@@ -35,6 +43,12 @@ class Landing extends Component {
       video: currVid
     });
   }
+  renderStream=()=>{
+    let streams = stream.map(vid=>{
+      return <StreamGame links={vid}/>
+    })
+    return streams;
+  }
 
   render() {
     return (
@@ -44,10 +58,27 @@ class Landing extends Component {
           <Video link={links[this.state.video]}/>
           <Button onClick={()=>this.handleNext()} color="black" icon='right chevron'/>
         </div>
+        <p className="top-stream">Top Streamers</p>
+        <div className="stream-games">
+          {this.renderStream()}
+        </div>
+        <p className="top-stream">Top Games</p>
         <div className="top-games">
-          <div className ="fort-box"></div>
-          <div className ="apex-box"></div>
-          <div className ="sekiro-box"></div>
+          <div className="view-border">
+            <div className ="fort-box"></div>
+            <p className="game-name">{Faker.lorem.word()}</p>
+            <p className="view">{Faker.random.number()} Views </p>
+          </div>
+          <div className="view-border">
+            <div className ="apex-box"></div>
+            <p className="game-name">{Faker.lorem.word()}</p>
+            <p className="view">{Faker.random.number()} Views </p>
+          </div>
+          <div className="view-border">
+            <div className ="sekiro-box"></div>
+            <p className="game-name">{Faker.lorem.word()}</p>
+            <p className="view">{Faker.random.number()} Views </p>
+          </div>
         </div>
       </div>
     );
